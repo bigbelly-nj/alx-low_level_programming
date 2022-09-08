7e17a4602ed5663a874ac214108ac44c4a8dcf43
bigbelly-nj <nonsojohn09917@gmail.com>
2022-09-08 02:36:29 -0700

    low level prog

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - Print a string
- *   * Description: use printf
- *    * Return: 0
+ * main - Prints a string
+ * Description : use printf
+ * Return : 0
  */
 int main(void)
 {
